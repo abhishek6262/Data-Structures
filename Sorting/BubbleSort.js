67f1fdf40ec49fb46d34ea8503f74bc1c442c926
Abhishek Prakash <prakashabhishek6262@gmail.com>
2022-08-18 19:25:00 +0000

    Add flag

--- Sorting/BubbleSort.js
+++ Sorting/BubbleSort.js
@@ -2,11 +2,14 @@ let nums = [1, 88, 9, 55, 65, 8, 5, 2, 12, 64, 73];
 
 function bubbleSort(nums) {
     for (let i = 0; i < nums.length; i++) {
+        let noSwap = true;
         for (let j = 0; j < nums.length - i - 1; j++) {
             if (nums[j] > nums[j + 1]) {
                 [nums[j + 1], nums[j]] = [nums[j], nums[j + 1]];
+                noSwap = false;
             }
         }
+        if (noSwap) break;
     }
 
     return nums;
