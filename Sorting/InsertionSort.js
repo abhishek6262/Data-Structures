75d0c2c726d7834dd84544bf089b34f94b1d1495
Abhishek Prakash <prakashabhishek6262@gmail.com>
2022-08-18 19:15:03 +0000

    Add insertion sort

---
+++ Sorting/InsertionSort.js
@@ -0,0 +1,17 @@
+let nums = [1, 88, 9, 55, 65, 8, 5, 2, 12, 64, 73];
+
+function insertionSort(nums) {
+    for (let i = 1; i < nums.length; i++) {
+        const num = nums[i];
+        let j = i - 1;
+
+        while (j >= 0 && nums[j] > num) nums[j + 1] = nums[j--];
+
+        nums[j + 1] = num;
+    }
+
+
+    return nums;
+}
+
+console.log(insertionSort(nums));
